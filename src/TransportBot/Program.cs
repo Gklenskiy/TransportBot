919f7b31664ec95fd2591ce7c4da851f9d329fd6
GKlenskiy <g.klensky@gmail.com>
2018-04-08 15:54:57 +0300

    add project and packages

---
+++ src/TransportBot/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace TransportBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "TransportBot";
+
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.Build()
+				.Run();
+		}
+	}
+}
---
+++ src/TransportBot/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace TransportBot
+{
+	public class Startup
+	{
+		private readonly IConfigurationRoot _configuration;
+
+		public Startup(IHostingEnvironment env)
+		{
+			_configuration = BuildConfiguration(env.ContentRootPath)
+				.AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+				.Build();
+		}
+
+		public static IConfigurationBuilder BuildConfiguration(string contentRoot)
+		{
+			return new ConfigurationBuilder()
+				.SetBasePath(contentRoot)
+				.AddEnvironmentVariables("TransportBot_")
+				.AddJsonFile("appsettings.json");
+		}
+	}
+}
